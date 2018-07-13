3eb6e43151c36bceccdba9c10542f124e53d68be
github username <github email address>
2018-07-13 15:39:13 +0100

    Done.

--- index.js
+++ index.js
@@ -6,4 +6,17 @@ Variables defined in the test suite:
 You should NOT try to redefine any of the above variables.
 */
 
-// Write your code below this line!
+
+let newID = oldID + 1000000000
+
+let ageIsValid = Number.isInteger(currentAge)
+
+let randomNumber = Math.random(0, 20)
+
+let randomInteger = Math.floor(randomNumber);
+console.log(randomInteger)
+let randomUserID = randomInteger + 1000000001
+
+console.log(randomInteger);
+
+// let randomUserID = parseInt(randomInteger + "1000000000")
